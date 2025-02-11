acc21bd2cb4695e62b76c7d5a198ee6e6e18283f
Marco Coelho <marcoelho97@gmail.com>
2025-02-11 03:15:21 +0000

    Formatting

--- src/components/Sections/OpinionArticles/OpinionArticles.jsx
+++ src/components/Sections/OpinionArticles/OpinionArticles.jsx
@@ -1,5 +1,5 @@
-import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import Slider from "react-slick";
 import ImageOpinionCardCol from "../../Cards/ImageOpinionCardCol";
 import OpinionArticlesCSS from "./OpinionArticles.module.css";
